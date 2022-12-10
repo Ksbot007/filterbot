14e6f7519cbed8d85d3c0b34f9e4862919970369
Kunal <91754021+Ksbot007@users.noreply.github.com>
2022-12-11 01:33:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -89,8 +89,7 @@ async def start(client, message):
             InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=random.choice(PICS),
+        await message.reply_caption(
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
