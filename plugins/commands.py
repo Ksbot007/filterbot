e16297e4f5ff788e02a1bf81be92a13d430b722c
Kunal <91754021+Ksbot007@users.noreply.github.com>
2022-12-11 12:19:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,6 +23,10 @@ async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [
             [
+                InlineKeyboardButton(' CHANNEL ', url='https://t.me/MKVNETWORKS'),
+                InlineKeyboardButton(' GROUP ', url="https://t.me/MKVGROUPS"),
+            ],
+            [
                 InlineKeyboardButton(text=DOWNLOAD_TEXT_NAME,url=DOWNLOAD_TEXT_URL)
             ]
             ]
@@ -39,16 +43,15 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help')
-        ]]
+            InlineKeyboardButton(' CHANNEL ', url='https://t.me/MKVNETWORKS'),
+            InlineKeyboardButton(' GROUP ', url='https://t.me/MKVGROUPS')
+            ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_caption(
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+        await message.reply_text(
+            text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
-        )
+            )
         return
     if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
