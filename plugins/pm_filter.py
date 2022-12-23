c2a038305ca97afe29ee1af0876a34d99198c6e1
Kunal <91754021+Ksbot007@users.noreply.github.com>
2022-12-23 15:44:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -738,7 +738,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"<b>Here is What I Found In My Database For Your Query <code>{search}</code>"
+        cap = f"<b>Here is What I Found In My Database For Your Query <code>{search}</code></b>"
     if imdb and imdb.get('poster'):
         try:
             hehe =  await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
