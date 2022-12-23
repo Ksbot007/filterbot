54c24923009f508ee849bdeed1a189d8d615babe
Kunal <91754021+Ksbot007@users.noreply.github.com>
2022-12-23 18:32:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -37,7 +37,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [
-            InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton("⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚", url=f'http://t.me/{temp.U_NAME}?startgroup=true')
            ]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_text(
